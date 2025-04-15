cf6a1367fb6bb107edbed9b32eb17bca4e0d2759
Moe <triquetmoe@gmail.com>
2025-04-15 16:01:21 +0200

    Initialisation SpringBoot + Architecrure Service de base

---
+++ taskservice/src/main/java/com/example/taskservice/model/Task.java
@@ -0,0 +1,49 @@
+package com.example.taskservice.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private boolean completed;
+
+    // Constructeurs
+    public Task() {
+    }
+
+    public Task(String title) {
+        this.title = title;
+        this.completed = false;
+    }
+
+    // Getters & Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}z
